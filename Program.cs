e333a05d60035d75345f35b7d5d159d6265630d5
Radhika Bhala <radhabhala1@gmail.com>
2018-02-27 15:37:06 -0800

    entire project

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+using AzureInterface;
+using System.Globalization;
+
+namespace WeatherStationReader
+{
+    class Program
+    {
+        static string WUKey = "";
+        const string RecordTable = "WeatherData";
+        static void Main(string[] args)
+        {
+            try
+            {
+                string weatherRequest = CreateRequest("KCASANAR3.json");
+                string response = MakeRequest(weatherRequest);
+                WeatherData wd = ConvertJson(response);
+                UploadToAzure(wd);
+                Console.WriteLine("uploaded: " + wd.Time);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+
+        }
+
+        //Create the request URL
+        public static string CreateRequest(string queryString)
+        {
+            string UrlRequest = "http://api.wunderground.com/api/" +
+                                           WUKey +
+                                           "/conditions/q/pws:" + queryString;
+            return (UrlRequest);
+        }
+
+        public static String MakeRequest(string requestUrl)
+        {
+            try
+            {
+                HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(requestUrl);
+                WebResponse myResp = myReq.GetResponse();
+                using (StreamReader reader = new StreamReader(myResp.GetResponseStream()))
+                {
+                    string jsonString = reader.ReadToEnd();
+                    dynamic results = JsonConvert.DeserializeObject<dynamic>(jsonString);
+
+                    return jsonString;
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+        public static WeatherData ConvertJson(string response)
+        {
+            WeatherData wd = new WeatherData();
+            dynamic results = JsonConvert.DeserializeObject<dynamic>(response);
+            wd.StationId = results.current_observation.station_id;
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            wd.Time = DateTime.ParseExact(results.current_observation.observation_time_rfc822.ToString(), "ddd, dd MMM yyyy HH:mm:ss zzz", provider);
+            wd.weather = results.current_observation.weather;
+            wd.Temprature = results.current_observation.temp_f;
+            wd.RelativeHumidity = results.current_observation.relative_humidity;
+            wd.WindDirection = results.current_observation.wind_dir;
+            wd.WindDegrees = results.current_observation.wind_degrees;
+            wd.WindMPH = results.current_observation.wind_mph;
+            wd.WindGustMPH = results.current_observation.wind_gust_mph;
+            wd.Pressure = results.current_observation.pressure_mb;
+            wd.PressureTrend = results.current_observation.pressure_trend;
+            wd.DewPoint = results.current_observation.dewpoint_f;
+            wd.HeatIndex = results.current_observation.heat_index_f;
+            wd.FeelsLikeTemp = results.current_observation.feelslike_f;
+            wd.VisibilityDistance = results.current_observation.visibility_mi;
+            wd.SolarRadiation = results.current_observation.solarradiation;
+            wd.UV = results.current_observation.UV;
+            wd.PrecipitationInHr = results.current_observation.precip_1hr_in;
+            wd.PrecipitationToday = results.current_observation.precip_today_in;
+            wd.WindChill = results.current_observation.windchill_f;
+            wd.SetTableKeys();
+            return wd;
+
+        }
+
+        public static void UploadToAzure(WeatherData wd)
+        {
+            string SASKey = "";
+
+            TableUploader<WeatherData> uploader = new TableUploader<WeatherData>(SASKey);
+            uploader.Upload(wd);
+        }
+        
+    }
+}
+
+
+
---
+++ WeatherData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Amber.Flywheel;
+using Amber.Protobuf;
+
+namespace WeatherStationReader
+{
+    public class WeatherData : TableEntity
+    {
+        public string StationId { get; set; }
+        public DateTime Time { get; set; }
+        public string weather { get; set; }
+        public double Temprature { get; set; }
+        public string RelativeHumidity { get; set; }
+        public string WindDirection { get; set; }
+        public string WindDegrees { get; set; }
+        public string WindMPH { get; set; }
+        public string WindGustMPH { get; set; }
+        public string Pressure { get; set; }
+        public string PressureTrend { get; set; }
+        public string DewPoint { get; set; }
+        public string HeatIndex { get; set; }
+        public string FeelsLikeTemp { get; set; }
+        public string VisibilityDistance { get; set; }
+        public string SolarRadiation { get; set; }
+        public string UV { get; set; }
+        public string PrecipitationInHr { get; set; }
+        public string PrecipitationToday { get; set; }
+        public string WindChill { get; set; }
+
+        public WeatherData()
+        {
+            PartitionKey = "";
+            RowKey = "";
+        }
+        public void SetTableKeys()
+        {
+            PartitionKey = this.StationId;
+            RowKey = ShimUtils.ConvertDateTimeToRowKey(this.Time);
+        }
+    }
+
+
+}
